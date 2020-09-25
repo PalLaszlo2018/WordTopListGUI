ea7e3df53721936b51499264e203da5bd07f8103
laszlop <laszlop@pcdev15d4>
2020-09-25 16:03:56 +0200

    add license

--- WordTopListGUI/src/wordtoplistgui/sorters/WordStore.java
+++ WordTopListGUI/src/wordtoplistgui/sorters/WordStore.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright: SONY MUSIC ENTERTAINMENT 
+ * Create Date: 2020.09.25. 
  */
 package wordtoplistgui.sorters;
 
